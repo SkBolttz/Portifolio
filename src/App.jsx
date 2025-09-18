ac9b6bdb71ae60d2186d6ba06461ac01d0ec1281
henrique <37087436+SkBolttz@users.noreply.github.com>
2025-09-17 23:00:07 -0300

    Ajuste no header e responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -86,209 +86,225 @@ const handleAnimationComplete = () => {};
 function App() {
   return (
     <div>
-      <Header />
-      <div className="bg-[#3B2740] flex items-center justify-between relative px-8 md:px-20 py-16">
-        <div className="absolute inset-0 z-0">
-          <BackgroundLines svgOptions={{}} />
-        </div>
-        <div className="relative z-10 flex-1 px-64">
-          <SplitText
-            text="Pedro Henrique"
-            className="text-[80px] text-[#6E3E7D] font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]"
-            delay={100}
-            duration={1.5}
-            ease="power3.out"
-            splitType="chars"
-            from={{ opacity: 0, y: 40 }}
-            to={{ opacity: 1, y: 0 }}
-            threshold={0.1}
-            rootMargin="-100px"
-            textAlign="center"
-            onLetterAnimationComplete={handleAnimationComplete}
-          />
-          <SplitText
-            text="de Borba"
-            className="text-[40px] text-[#6E3E7D] font-montserrat"
-            delay={200}
-            duration={1.5}
-            ease="power3.out"
-            splitType="chars"
-            from={{ opacity: 0, y: 40 }}
-            to={{ opacity: 1, y: 0 }}
-            threshold={0.1}
-            rootMargin="-100px"
-            textAlign="center"
-            onLetterAnimationComplete={handleAnimationComplete}
-          />
-          <br />
-          <br />
-          <div className="translate-y-16">
-            <h2 className="text-[30px] text-white font-bold font-montserrat">
-              Desenvolvedor de Software
-            </h2>
-            <h3 className="text-[45px] text-[#A04FB0] font-bold font-montserrat">
-              <GradientMarqueeText texto="FullStack" />
-            </h3>
-            <p className="text-[#B3B3B3] font-montserrat py-4 text-[25px]">
-              Estudante de Ciências da Computação
-            </p>
-            <p className="text-[#B3B3B3] font-montserrat text-[25px] translate-y-8">
-              Atualmente atuando como estagiário na <br /> empresa Phillips como
-              suporte técnico.
-            </p>
+      {/* Header */}
+      <div id="inicio">
+        <Header />
+        <div className="bg-[#3B2740] flex flex-col lg:flex-row items-center justify-between relative px-8 md:px-20 py-16">
+          <div className="absolute inset-0 z-0">
+            <BackgroundLines svgOptions={{}} />
           </div>
-          <div className="py-20 mt-32">
-            <BotaoCircular texto="Conhecer mais ↓" />
+
+          <div className="relative z-10 flex-1 px-4 lg:px-64 text-center lg:text-left">
+            <SplitText
+              text="Pedro Henrique"
+              className="text-[50px] sm:text-[60px] md:text-[80px] text-[#6E3E7D] font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]"
+              delay={100}
+              duration={1.5}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+              onLetterAnimationComplete={handleAnimationComplete}
+            />
+            <SplitText
+              text="de Borba"
+              className="text-[30px] sm:text-[35px] md:text-[40px] text-[#6E3E7D] font-montserrat"
+              delay={200}
+              duration={1.5}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+              onLetterAnimationComplete={handleAnimationComplete}
+            />
+
+            <div className="translate-y-16">
+              <h2 className="text-[20px] sm:text-[25px] md:text-[30px] text-white font-bold font-montserrat">
+                Desenvolvedor de Software
+              </h2>
+              <h3 className="text-[30px] sm:text-[40px] md:text-[45px] text-[#A04FB0] font-bold font-montserrat">
+                <GradientMarqueeText texto="FullStack" />
+              </h3>
+              <p className="text-[#B3B3B3] font-montserrat py-4 text-[20px] sm:text-[23px] md:text-[25px]">
+                Estudante de Ciências da Computação
+              </p>
+              <p className="text-[#B3B3B3] font-montserrat text-[20px] sm:text-[23px] md:text-[25px] translate-y-8">
+                Atualmente atuando como estagiário na <br /> empresa Phillips
+                como suporte técnico.
+              </p>
+            </div>
+
+            <div className="py-20 mt-32">
+              <BotaoCircular texto="Conhecer mais ↓" />
+            </div>
           </div>
-        </div>
-        <div className="relative z-10 flex-shrink-0 w-1/3">
-          <CometCardDemo />
+
+          <div className="relative z-10 flex-shrink-0 w-full lg:w-1/3 flex justify-center mt-12 lg:mt-0">
+            <CometCardDemo />
+          </div>
         </div>
       </div>
 
-      <ScrollRevealAvancado
-        from="bottom"
-        delay={50}
-        duration={900}
-        stagger={50}
-      >
-        <div className="bg-[#1F1F1F]">
-          <div className="bg-[#1F1F1F] flex items-center justify-center relative px-8 md:px-20 py-16">
-            <div>
-              <h2 className="text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
-                Skills
-              </h2>
-            </div>
-          </div>
-          <div
-            style={{
-              height: "200px",
-              position: "relative",
-              overflow: "hidden",
-              padding: "0 10px",
-              marginBottom: "-100px",
-            }}
-          >
-            <LogoLoop
-              logos={imageLogos}
-              speed={120}
-              direction="left"
-              logoHeight={64}
-              gap={60}
-              pauseOnHover
-              scaleOnHover
-              fadeOut
-              fadeOutColor="#0A0A0A"
-              ariaLabel="Technology partners"
-            />
-          </div>
-          <div className="bg-[#1F1F1F] flex justify-between px-8 md:px-20 py-16">
-            <div>
-              <CardHoverEffectDemo />
-            </div>
-            <div>
-              <CardHoverEffectDemo2 />
-            </div>
+      {/* Skills */}
+      <div id="skills" className="bg-[#1F1F1F]">
+        <div className="flex items-center justify-center relative px-8 md:px-20 py-16">
+          <div>
+            <h2 className="text-[40px] sm:text-[45px] md:text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
+              Skills
+            </h2>
           </div>
         </div>
-        <div className="bg-[#121212] flex items-center justify-center relative py-14">
-          <div className="text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
-            <h2>Projetos</h2>
-          </div>
-        </div>
-      </ScrollRevealAvancado>
 
-      <ScrollRevealAvancado
-        from="bottom"
-        delay={50}
-        duration={900}
-        stagger={50}
-      >
-        <div className="bg-[#121212] flex items-center justify-center relative -translate-z-24">
-          <p className="text-[20px] text-[#B3B3B3] font-montserrat">
-            Alguns dos projetos que desenvolvi ao longo da minha jornada como
-            desenvolvedor
-          </p>
+        <div
+          style={{
+            height: "200px",
+            position: "relative",
+            overflow: "hidden",
+            padding: "0 10px",
+            marginBottom: "-100px",
+          }}
+        >
+          <LogoLoop
+            logos={imageLogos}
+            speed={120}
+            direction="left"
+            logoHeight={64}
+            gap={60}
+            pauseOnHover
+            scaleOnHover
+            fadeOut
+            fadeOutColor="#0A0A0A"
+            ariaLabel="Technology partners"
+          />
         </div>
-        <div className="bg-[#121212] flex justify-center py-14">
-          <BackgroundGradientDemo />
+
+        <div className="flex flex-col md:flex-row justify-between px-8 md:px-20 py-16 gap-8 md:gap-0">
+          <CardHoverEffectDemo />
+          <CardHoverEffectDemo2 />
         </div>
-      </ScrollRevealAvancado>
-      <div className="bg-[#1F1F1F]">
+      </div>
+
+      {/* Projetos */}
+      <section id="projetos" className="bg-[#121212]">
         <ScrollRevealAvancado
           from="bottom"
           delay={50}
           duration={900}
           stagger={50}
         >
-          <div className="flex justify-center px-8 md:px-20 py-16">
-            <h1 className="text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
-              🎓 Meus Certificados em Programação 🚀
-            </h1>
-          </div>
-          <div className="flex justify-center mt-10">
-            <div className="text-center text-[#B3B3B3] text-sm sm:text-base mt-10 -translate-y-28">
-              <p className="text-[20px]">
-                Bem-vindo ao meu repositório! Aqui você vai encontrar uma
-                coleção dos meus <br /> certificados nas áreas de programação,
-                incluindo Front-end, Back-end e muito mais!
-                <br />
-                Vamos lá? 💻✨
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center py-16">
-            <div className="w-full max-w-7xl px-6 mb-20">
-              <h2 className="text-3xl md:text-4xl text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2] mb-8 text-center -translate-x-96">
-                🌟 Front-end Development
-              </h2>
-              <div className="w-full text-white text-sm sm:text-base">
-                <CertificadosFront />
-              </div>
-            </div>
-
-            <div className="w-full max-w-7xl px-6 mb-20">
-              <h2 className="text-3xl md:text-4xl text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2] mb-8 text-center -translate-x-96">
-                🔙 Back-end Development
-              </h2>
-              <div className="w-full text-white text-sm sm:text-base">
-                <CertificadosBack />
-              </div>
-            </div>
-
-            <div className="w-full max-w-7xl px-6">
-              <h2 className="text-3xl md:text-4xl text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2] mb-8 text-center -translate-x-96">
-                📚 Outros Certificados
-              </h2>
-              <div className="w-full text-white text-sm sm:text-base">
-                <OutrosCertificados />
-              </div>
+          <div className="flex items-center justify-center relative py-14">
+            <div className="text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
+              <h2>Projetos</h2>
             </div>
           </div>
         </ScrollRevealAvancado>
+
+        <ScrollRevealAvancado
+          from="bottom"
+          delay={50}
+          duration={900}
+          stagger={50}
+        >
+          <div className="flex items-center justify-center relative -translate-z-24">
+            <p className="text-[20px] text-[#B3B3B3] font-montserrat">
+              Alguns dos projetos que desenvolvi ao longo da minha jornada como
+              desenvolvedor
+            </p>
+          </div>
+
+          <div className="flex justify-center py-14">
+            <BackgroundGradientDemo />
+          </div>
+        </ScrollRevealAvancado>
+      </section>
+
+      {/* Projetos */}
+      <div id="certificados">
+        <div className="bg-[#1F1F1F]">
+          <ScrollRevealAvancado
+            from="bottom"
+            delay={50}
+            duration={900}
+            stagger={50}
+          >
+            <div className="flex justify-center px-8 md:px-20 py-16">
+              <h1 className="text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
+                🎓 Meus Certificados em Programação 🚀
+              </h1>
+            </div>
+            <div className="flex justify-center mt-10">
+              <div className="text-center text-[#B3B3B3] text-sm sm:text-base mt-10 -translate-y-28">
+                <p className="text-[20px]">
+                  Bem-vindo ao meu repositório! Aqui você vai encontrar uma
+                  coleção dos meus <br /> certificados nas áreas de programação,
+                  incluindo Front-end, Back-end e muito mais!
+                  <br />
+                  Vamos lá? 💻✨
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center py-16">
+              <div className="w-full max-w-7xl px-6 mb-20">
+                <h2 className="text-3xl md:text-4xl text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2] mb-8 text-center -translate-x-96">
+                  🌟 Front-end Development
+                </h2>
+                <div className="w-full text-white text-sm sm:text-base">
+                  <CertificadosFront />
+                </div>
+              </div>
+
+              <div className="w-full max-w-7xl px-6 mb-20">
+                <h2 className="text-3xl md:text-4xl text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2] mb-8 text-center -translate-x-96">
+                  🔙 Back-end Development
+                </h2>
+                <div className="w-full text-white text-sm sm:text-base">
+                  <CertificadosBack />
+                </div>
+              </div>
+
+              <div className="w-full max-w-7xl px-6">
+                <h2 className="text-3xl md:text-4xl text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2] mb-8 text-center -translate-x-96">
+                  📚 Outros Certificados
+                </h2>
+                <div className="w-full text-white text-sm sm:text-base">
+                  <OutrosCertificados />
+                </div>
+              </div>
+            </div>
+          </ScrollRevealAvancado>
+        </div>
       </div>
+
       <ScrollRevealAvancado
         from="bottom"
         delay={50}
         duration={900}
         stagger={50}
       >
-        <div className="bg-[#1F1F1F] pb-20">
-          <div>
-            <div className="flex justify-center py-14">
-              <h1 className="text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
-                Contato
-              </h1>
-            </div>
-            <div className="flex justify-center mt-10">
-              <div className="text-center text-[#B3B3B3] text-sm sm:text-base mt-10 -translate-y-28">
-                <p className="text-[32px]">
-                  Vamos conversar sobre oportunidades e projetos interessantes
-                </p>
-              </div>
-            </div>
+        <div id="contatos">
+          <div className="bg-[#1F1F1F] pb-20">
             <div>
-              <ContactForm />
+              <div className="flex justify-center py-14">
+                <h1 className="text-[50px] text-white font-bold font-montserrat drop-shadow-[0_0_15px_#8A2BE2]">
+                  Contato
+                </h1>
+              </div>
+              <div className="flex justify-center mt-10">
+                <div className="text-center text-[#B3B3B3] text-sm sm:text-base mt-10 -translate-y-28">
+                  <p className="text-[32px]">
+                    Vamos conversar sobre oportunidades e projetos interessantes
+                  </p>
+                </div>
+              </div>
+              <div>
+                <ContactForm />
+              </div>
             </div>
           </div>
         </div>
--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,21 +1,40 @@
 import { Link } from "react-router-dom";
 import "../../index.css";
 import Botao from "../Ui/Botao";
+import TextType from "../Ui/TextType";
 
 export default function Header() {
   return (
     <>
-      <header className="fixed top-0 w-full z-50 bg-[#6E3E7D] py-5">
-        <nav className="flex justify-between items-center px-80">
-          <div className="text-[30px] text-black font-bold font-montserrat">
-            Portfólio
-          </div>
+      <header className="fixed top-0 w-full z-50 bg-[#6E3E7D] py-3">
+        <nav className="flex justify-between px-80">
+          <TextType
+            text={[
+              "Transformando ideias em código.",
+              "Criando soluções que fazem a diferença.",
+            ]}
+            typingSpeed={75}
+            pauseDuration={3000}
+            showCursor={true}
+            cursorCharacter="|"
+            className="text-[32px] text-[#121212] font-bold font-montserrat"
+          />
           <nav className="flex gap-4">
-            <Botao texto="Início" />
-            <Botao texto="Skills" />
-            <Botao texto="Projetos" />
-            <Botao texto="Certificados" />
-            <Botao texto="Contatos" />
+            <a href="#inicio">
+              <Botao texto="Início" />
+            </a>
+            <a href="#skills">
+              <Botao texto="Skills" />
+            </a>
+            <a href="#projetos">
+              <Botao texto="Projetos" />
+            </a>
+            <a href="#certificados">
+              <Botao texto="Certificados" />
+            </a>
+            <a href="#contatos">
+              <Botao texto="Contatos" />
+            </a>
           </nav>
         </nav>
       </header>
